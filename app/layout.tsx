5aea2c71429774d6f95be692e3d05d63fbaec963
Arun <aj7433413@gmail.com>
2024-01-06 15:46:06 +0530

    added contact form

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import StarsCanvas from "@/components/main/StarBackground";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Space Portfolio",
+  title: "Arun's Portfolio",
   description: "This is my portfolio",
 };
 
