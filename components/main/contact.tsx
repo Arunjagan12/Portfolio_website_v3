780547c23451939098a13a3e1a4a082276f9f66a
Arun <aj7433413@gmail.com>
2024-01-07 16:21:55 +0530

    modified contact form

--- components/main/contact.tsx
+++ components/main/contact.tsx
@@ -3,13 +3,29 @@ import React from 'react';
 const Contact = () => {
   return (
     <section>
-      <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md border  border-[#2A0E61] rounded-lg  z-50">
+      <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md border shadow border-[#2A0E61] rounded-lg  z-50">
       <h2 className="text-[45px] text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 font-bold mt-[10px] text-center mb-[15px]">
-          Contact Us
+          Contact Me
         </h2>
       
         
-        <form action="#" className="space-y-8">
+        <form method="post" name="contact_form"
+              className="space-y-8">
+          <div>
+            <label
+              htmlFor="name"
+              className="block mb-2 text-sm font-medium text-cyan-500"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
+              placeholder="Enter your Name"
+              required
+            />
+          </div>
           <div>
             <label
               htmlFor="email"
@@ -19,7 +35,7 @@ const Contact = () => {
             </label>
             <input
               type="email"
-              id="email"
+              name="email"
               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
               placeholder="Enter your Mail ID"
               required
@@ -34,7 +50,7 @@ const Contact = () => {
             </label>
             <input
               type="text"
-              id="subject"
+              name="subject"
               className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500"
               placeholder="Let us know how we can help you"
               required
@@ -48,7 +64,7 @@ const Contact = () => {
               Your message
             </label>
             <textarea
-              id="message"
+              name="message"
               rows={6}
               className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
               placeholder="Leave a comment..."
@@ -56,13 +72,15 @@ const Contact = () => {
           </div>
           <button
             type="submit"
-            className="py-2 border  border-[#2A0E61] w-full text-center text-white cursor-pointer rounded-lg"
+            value="submit"
+            className="py-2 border  border-[#2A0E61] w-full text-center hover:bg-cyan-600 text-white cursor-pointer rounded-lg"
           >
             Send message
           </button>
         </form>
       </div>
     </section>
+  
   );
 };
 
